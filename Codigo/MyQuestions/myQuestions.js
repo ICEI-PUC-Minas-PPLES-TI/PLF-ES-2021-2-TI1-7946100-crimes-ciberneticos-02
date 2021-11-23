db631ffa76e385b4e4705c18ce581870b8a11997
Luiz Felipe Vieira <luizfelipevieira01@gmail.com>
2021-11-23 18:29:33 -0300

    UPDATE gestão quizz js

--- Codigo/MyQuestions/myQuestions.js
+++ Codigo/MyQuestions/myQuestions.js
@@ -4,16 +4,24 @@ var db_perguntaQuizzStdin = {
     {
       id: 1,
       pergunta: "Você conhece o 2FA ?",
-      resposta:
+      dica:
         "A autenticação de dois fatores é uma camada extra de proteção que pode ser ativada em contas online. Também conhecido pela sigla 2FA, originária do inglês 'two-factor authentication', o recurso insere uma segunda verificação de identidade do usuário no momento do login, evitando o acesso às contas mesmo quando a senha é vazada.",
-      alternativa: true,
+      alternativaA: "sim",
+      alternativaB: "sim",
+      alternativaC: "sim",
+      alternativaD: "sim",
+      alternativaCorreta: "A",
       site: "encurtador.com.br/myN38",
     },
     {
       id: 2,
       pergunta: "Você conhece o registrato do Bano cental ?",
-      resposta: "É uma ferramenta que permite o cidadão saber Informações gratuitas de dívidas com bancos e órgãos públicos, cheques devolvidos, contas, chaves Pix e operações de câmbio.",
-      alternativa: true,
+      dica: "É uma ferramenta que permite o cidadão saber Informações gratuitas de dívidas com bancos e órgãos públicos, cheques devolvidos, contas, chaves Pix e operações de câmbio.",
+      alternativaA: "sim",
+      alternativaB: "sim",
+      alternativaC: "sim",
+      alternativaD: "sim",
+      alternativaCorreta: "A",
       site: "encurtador.com.br/sDL46",
     },
   ],
@@ -34,13 +42,18 @@ function insertPergunta(perguntaQuizz) {
   // Calcula novo Id a partir do último código existente no array (PODE GERAR ERRO SE A BASE ESTIVER VAZIA)
   let novoId = 1;
   if (db.data.length != 0) novoId = db.data[db.data.length - 1].id + 1;
-  let novaPergunta = {
+  let novaPergunta = 
+    {
     id: novoId,
     pergunta: perguntaQuizz.pergunta,
-    resposta: perguntaQuizz.resposta,
-    alternativa: perguntaQuizz.alternativa,
+    dica: perguntaQuizz.dica,
+    alternativaA: perguntaQuizz.alternativaA,
+    alternativaB: perguntaQuizz.alternativaB,
+    alternativaC: perguntaQuizz.alternativaC,
+    alternativaD: perguntaQuizz.alternativaD,
+    alternativaCorreta: perguntaQuizz.alternativaCorreta,
     site: perguntaQuizz.site,
-  };
+    };
 
   // Insere o novo objeto no array
   db.data.push(novaPergunta);
@@ -55,9 +68,13 @@ function updatePergunta(id, perguntaQuizz) {
   let index = db.data.map((obj) => obj.id).indexOf(id);
 
   // Altera os dados do objeto no array
-  (db.data[index].pergunta = perguntaQuizz.pergunta),
-    (db.data[index].resposta = perguntaQuizz.resposta),
-    (db.data[index].alternativa = perguntaQuizz.alternativa),
+    (db.data[index].pergunta = perguntaQuizz.pergunta),
+    (db.data[index].dica = perguntaQuizz.dica),
+    (db.data[index].alternativaA = perguntaQuizz.alternativaA),
+    (db.data[index].alternativaB = perguntaQuizz.alternativaB),
+    (db.data[index].alternativaC = perguntaQuizz.alternativaC),
+    (db.data[index].alternativaD = perguntaQuizz.alternativaD),
+    (db.data[index].alternativaCorreta = perguntaQuizz.alternativaCorreta),
     (db.data[index].site = perguntaQuizz.site),
     displayMessage("Pergunta alterada com sucesso");
 
@@ -76,3 +93,30 @@ function deletePergunta(id) {
   // Atualiza os dados no Local Storage
   localStorage.setItem("db_quizz", JSON.stringify(db));
 }
+
+//Fazer passagem de objeto
+/*	id: 1,
+      	pergunta: "Você conhece o 2FA ?",
+      	dica: "A autenticação de dois fatores é uma camada extra de proteção que pode ser ativada em contas online. Também conhecido pela sigla 2FA, originária do inglês 'two-factor authentication', o recurso insere uma segunda verificação de identidade do usuário no momento do login, evitando o acesso às contas mesmo quando a senha é vazada.",
+      	resposta a: x
+	      resposta b: y
+	      resposta c: x
+	      resposta d: w
+	      correct:    y
+      	site: "encurtador.com.br/myN38",
+
+
+	let transfQuestion = 
+	{
+	question: perguntaQuizz.pergunta
+	answer: {
+        a: perguntaQuizz.alternativaA,
+        b: perguntaQuizz.alternativaB,
+	      c: perguntaQuizz.alternativaC,
+	      d: perguntaQuizz.alternativaD,
+      		},
+	correctAnswer: perguntaQuizz.correct,
+	},*/ 
+
+
+  
